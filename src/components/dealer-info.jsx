3c9e7dac564e062b3775f1245e07fd096739468f
paulCary123 <paulcarygf@gmail.com>
2024-07-12 09:13:42 -0400

    added vendor information

--- src/components/dealer-info.jsx
+++ src/components/dealer-info.jsx
@@ -60,135 +60,129 @@ const DealerInfo = ({ className = "", onClose }) => {
             </div>
           ) : (
             <div className="success-message">
-              <h2>Dealer Information!</h2>
+              <h2>Vendor Info</h2>
               <p>Show dealer info here....</p>
+              <section className="content3">
+              <div className="top3">              
+                <div className="h11 dealerinfores" >
+                  <div className="enter-your-password">
+                    Dealer Name:
+                  </div>             
+                  <div className="enter-your-password">
+                    ss{dealerInfo.retailerName}
+                  </div>
+                </div>
+                <div className="h11 dealerinfores" >
+                  <div className="enter-your-password">
+                  Dealer Company:
+                  </div>             
+                  <div className="enter-your-password">
+                    ss{dealerInfo.retailerCompany}
+                  </div>
+                </div>
+                <div className="h11 dealerinfores" >
+                  <div className="enter-your-password">
+                  Dealer City/State:
+                  </div>             
+                  <div className="enter-your-password">
+                    ss{dealerInfo.retailerCity}
+                  </div>
+                </div>
+                <div className="h11 dealerinfores" >
+                  <div className="enter-your-password">
+                  Dealer Contact No.:
+                  </div>             
+                  <div className="enter-your-password">
+                    ss{dealerInfo.retailerContactNo}
+                  </div>
+                </div>
+                <div className="h11 dealerinfores" >
+                  <div className="enter-your-password">
+                  Dealer Email:
+                  </div>             
+                  <div className="enter-your-password">
+                    ss{dealerInfo.retailerEmail}
+                  </div>
+                </div>
+                <div className="h11 dealerinfores" >
+                  <div className="enter-your-password">
+                  Dealer Lot number of the item:
+                  </div>             
+                  <div className="enter-your-password">
+                    {dealerInfo.retailerLotNo}
+                  </div>
+                </div>
+                <div className="h11 dealerinfores" >
+                  <div className="enter-your-password">
+                  Dealer Stock number of the item:
+                  </div>             
+                  <div className="enter-your-password">
+                    {dealerInfo.retailerStockNo}
+                  </div>
+                </div>
+                <div className="h11 dealerinfores" >
+                  <div className="enter-your-password">
+                  Wholesale Price:
+                  </div>             
+                  <div className="enter-your-password">
+                    {dealerInfo.wholesalePrice}
+                  </div>
+                </div>
+
+                <div className="h11 dealerinfores" >
+                  <div className="enter-your-password">
+                  Third Party:
+                  </div>             
+                  <div className="enter-your-password">
+                    {dealerInfo.thirdParty}
+                  </div>
+                </div>            
+                <div className="h11 dealerinfores" >
+                  <div className="enter-your-password">
+                  Diamond Id:
+                  </div>             
+                  <div className="enter-your-password">
+                    {dealerInfo.diamondID}
+                  </div>
+                </div>
+                <div className="h11 dealerinfores" >
+                  <div className="enter-your-password">
+                  Seller Name:
+                  </div>             
+                  <div className="enter-your-password">
+                    {dealerInfo.sellerName}
+                  </div>
+                </div>
+                <div className="h11 dealerinfores" >
+                  <div className="enter-your-password">
+                  Seller Address:
+                  </div>             
+                  <div className="enter-your-password">
+                    {dealerInfo.sellerAddress}
+                  </div>
+                </div>
+                <div className="h11 dealerinfores" >
+                  <div className="enter-your-password">
+                  Dealer Fax:
+                  </div>             
+                  <div className="enter-your-password">
+                    {dealerInfo.retailerFax}
+                  </div>
+                </div>
+                <div className="h11 dealerinfores" >
+                  <div className="enter-your-password">
+                  Dealer Address:
+                  </div>             
+                  <div className="enter-your-password">
+                    {dealerInfo.retailerAddress} 
+                  </div>
+                </div>
+              </div>
+              </section>
             </div>
           )}
         </div>
-      </section>
-       ) : (
-        <>
-        <section className="content3">
-          <div className="top3">
-          <div className="h11 ">
-            <h3 className="dealer-info1">Vendor Info</h3>            
-          </div>
-            <div className="h11 dealerinfores" >
-              <div className="enter-your-password">
-                 Dealer Name:
-              </div>             
-              <div className="enter-your-password">
-                ss{dealerInfo.retailerName}
-              </div>
-            </div>
-            <div className="h11 dealerinfores" >
-              <div className="enter-your-password">
-              Dealer Company:
-              </div>             
-              <div className="enter-your-password">
-                ss{dealerInfo.retailerCompany}
-              </div>
-            </div>
-            <div className="h11 dealerinfores" >
-              <div className="enter-your-password">
-              Dealer City/State:
-              </div>             
-              <div className="enter-your-password">
-                ss{dealerInfo.retailerCity}
-              </div>
-            </div>
-            <div className="h11 dealerinfores" >
-              <div className="enter-your-password">
-              Dealer Contact No.:
-              </div>             
-              <div className="enter-your-password">
-                ss{dealerInfo.retailerContactNo}
-              </div>
-            </div>
-            <div className="h11 dealerinfores" >
-              <div className="enter-your-password">
-              Dealer Email:
-              </div>             
-              <div className="enter-your-password">
-                ss{dealerInfo.retailerEmail}
-              </div>
-            </div>
-            <div className="h11 dealerinfores" >
-              <div className="enter-your-password">
-              Dealer Lot number of the item:
-              </div>             
-              <div className="enter-your-password">
-                {dealerInfo.retailerLotNo}
-              </div>
-            </div>
-            <div className="h11 dealerinfores" >
-              <div className="enter-your-password">
-              Dealer Stock number of the item:
-              </div>             
-              <div className="enter-your-password">
-                {dealerInfo.retailerStockNo}
-              </div>
-            </div>
-            <div className="h11 dealerinfores" >
-              <div className="enter-your-password">
-              Wholesale Price:
-              </div>             
-              <div className="enter-your-password">
-                {dealerInfo.wholesalePrice}
-              </div>
-            </div>
-
-            <div className="h11 dealerinfores" >
-              <div className="enter-your-password">
-              Third Party:
-              </div>             
-              <div className="enter-your-password">
-                {dealerInfo.thirdParty}
-              </div>
-            </div>            
-            <div className="h11 dealerinfores" >
-              <div className="enter-your-password">
-              Diamond Id:
-              </div>             
-              <div className="enter-your-password">
-                {dealerInfo.diamondID}
-              </div>
-            </div>
-            <div className="h11 dealerinfores" >
-              <div className="enter-your-password">
-              Seller Name:
-              </div>             
-              <div className="enter-your-password">
-                {dealerInfo.sellerName}
-              </div>
-            </div>
-            <div className="h11 dealerinfores" >
-              <div className="enter-your-password">
-              Seller Address:
-              </div>             
-              <div className="enter-your-password">
-                {dealerInfo.sellerAddress}
-              </div>
-            </div>
-            <div className="h11 dealerinfores" >
-              <div className="enter-your-password">
-              Dealer Fax:
-              </div>             
-              <div className="enter-your-password">
-                {dealerInfo.retailerFax}
-              </div>
-            </div>
-            <div className="h11 dealerinfores" >
-              <div className="enter-your-password">
-              Dealer Address:
-              </div>             
-              <div className="enter-your-password">
-                {dealerInfo.retailerAddress} 
-              </div>
-            </div>
-          </div>
-        </section>
-      </>)}    
+      </section>          
       <img 
         className="close-icon2" 
         loading="lazy" 
