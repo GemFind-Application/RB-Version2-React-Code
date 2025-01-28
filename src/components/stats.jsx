1f8e7aded3c683b2ffa881a5776eda7be1593614
paulCary123 <paulcarygf@gmail.com>
2025-01-27 23:08:53 -0500

    changes for showing options according to setting

--- src/components/stats.jsx
+++ src/components/stats.jsx
@@ -1,29 +1,29 @@
-import React, { useState, useRef ,useEffect,forwardRef} from "react";
+import React, { useState, useRef, useEffect, forwardRef } from "react";
 import PropTypes from "prop-types";
 import "./stats.css";
 import { useReactToPrint } from 'react-to-print';
 const imageUrl = `${import.meta.env.VITE_IMAGE_URL}`;
 //const  ComponentToPrint=()=>{
- //class ComponentToPrint extends React.PureComponent {
-  const ComponentToPrint = forwardRef(({data}, ref) =>{
-    var parser = new DOMParser();
-     var doc = parser.parseFromString(data, 'text/html');
-    // const myDiv = doc.querySelector('.printDiv');
-    //const RenderHTML = (props) => (<span dangerouslySetInnerHTML={{__html:props.HTML}}></span>)
-      return (
-         <div  ref={ref} dangerouslySetInnerHTML={{__html:data}}/>
-      );
-    }
-  
-  )
- 
-const Stats = ({ 
+//class ComponentToPrint extends React.PureComponent {
+const ComponentToPrint = forwardRef(({ data }, ref) => {
+  var parser = new DOMParser();
+  var doc = parser.parseFromString(data, 'text/html');
+  // const myDiv = doc.querySelector('.printDiv');
+  //const RenderHTML = (props) => (<span dangerouslySetInnerHTML={{__html:props.HTML}}></span>)
+  return (
+    <div ref={ref} dangerouslySetInnerHTML={{ __html: data }} />
+  );
+}
+
+)
+
+const Stats = ({
   formSetting,
   configAppData,
-  emailAFriend, 
-  openDropHint, 
-  openScheduleViewing, 
-  openRequestInfo ,
+  emailAFriend,
+  openDropHint,
+  openScheduleViewing,
+  openRequestInfo,
   openPrintRequest,
   diamondContent,
   showPrint
@@ -36,49 +36,49 @@ const Stats = ({
   });
   useEffect(() => {
     const input = componentRef.current;
-   //input.select();
+    //input.select();
   }, []);
-//console.log(configAppData)
+  //console.log(configAppData)
   return (
     <div className="details4Info">
-      {configAppData.enable_hint &&
-      <div className="stat-items" onClick={openDropHint}>
-        <div className="stat-item dropAHint">
-          <b>Drop A Hint</b>
+      {configAppData.enable_hint && configAppData.enable_hint == "true" &&
+        <div className="stat-items" onClick={openDropHint}>
+          <div className="stat-item dropAHint">
+            <b>Drop A Hint</b>
+          </div>
         </div>
-      </div>
       }
-      {configAppData.enable_schedule_viewing &&
-      <div className="stat-items" onClick={openScheduleViewing}>
-        <div className="stat-item scheduleViewing">
-          <b>Schedule Viewing</b>
+      {configAppData.enable_schedule_viewing && configAppData.enable_schedule_viewing == "true" &&
+        <div className="stat-items" onClick={openScheduleViewing}>
+          <div className="stat-item scheduleViewing">
+            <b>Schedule Viewing</b>
+          </div>
         </div>
-      </div>
       }
-      {configAppData.enable_email_friend &&
-      <div className="stat-items" onClick={emailAFriend}>
-        <div className="stat-item emailAFriend">
-          <b>Email A Friend</b>
+      {configAppData.enable_email_friend && configAppData.enable_email_friend == "true" &&
+        <div className="stat-items" onClick={emailAFriend}>
+          <div className="stat-item emailAFriend">
+            <b>Email A Friend</b>
+          </div>
         </div>
-      </div>
       }
-      {configAppData.enable_more_info &&
-      <div className="stat-items" onClick={openRequestInfo}>
-        <div className="stat-item requestMoreInfo">
-          <b>Request More Info</b>
+      {configAppData.enable_more_info && configAppData.enable_more_info == "true" &&
+        <div className="stat-items" onClick={openRequestInfo}>
+          <div className="stat-item requestMoreInfo">
+            <b>Request More Info</b>
+          </div>
         </div>
-      </div>
       }
-      {showPrint && showPrint!==false &&
-      <div className="stat-items3" onClick={openPrintRequest}>
-            <div>
-            { <div style={{display:'none'}}> <ComponentToPrint ref={componentRef} data={[diamondContent?diamondContent:'']} /></div> }
+      {showPrint && showPrint !== false &&
+        <div className="stat-items3" onClick={openPrintRequest}>
+          <div>
+            {<div style={{ display: 'none' }}> <ComponentToPrint ref={componentRef} data={[diamondContent ? diamondContent : '']} /></div>}
 
             <div className="stat-item statPrint" onClick={handlePrint}>
               <b>Print</b>
             </div>
+          </div>
         </div>
-      </div>
       }
     </div>
   );
