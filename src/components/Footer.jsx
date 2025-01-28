8cb12e51689f4cb01c30a2e6ac5542a925bd2b52
paulCary123 <paulcarygf@gmail.com>
2025-01-28 12:36:16 -0500

    removed initial text

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-function Footer({configAppData}) {
+function Footer({ configAppData }) {
     return (
         <div className="pagination7">
             <div className="gemfind-app-store5">
-                © 2024 GemFind App Store {configAppData.show_powered_by !=='false' && 'Powered by GemFind'}.
+                {configAppData.show_powered_by == 'true' && 'Powered by GemFind'}.
             </div>
         </div>
     )
